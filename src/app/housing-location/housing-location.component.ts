3e9f4d6e9d50638f1fa7764b9e24375fa0d758e0
Andres Villavicencio <villadosa@hotmail.com>
2024-01-22 23:50:29 -0800

    feat: Listing housing locationn list

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HousingLocation } from '../housinglocation';
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HousingLocation} from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -8,12 +8,17 @@ import { HousingLocation } from '../housinglocation';
   imports: [CommonModule],
   template: `
     <section class="listing">
-      <img class="listing-photo">
-      <h2 class="listing-heading"></h2>
-      <p class="listing-location"></p>
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
     </section>
   `,
-  styleUrls: ['./housing-location.component.css']
+  styleUrls: ['./housing-location.component.css'],
 })
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
