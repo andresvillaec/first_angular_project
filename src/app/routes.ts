416b585c4104c1ebfc97173f53c60a629e871bec
Andres Villavicencio <villadosa@hotmail.com>
2024-01-29 20:24:28 -0500

    feat: Add detail route

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,18 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
+import { DetailsComponent } from "./details/details.component";
 
-const routeConfig: Routes = [];
+const routeConfig: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Homepage',
+    },
+    {
+        path: 'details/:id',
+        component: DetailsComponent,
+        title: 'Details page',
+    }
+];
 
 export default routeConfig;
