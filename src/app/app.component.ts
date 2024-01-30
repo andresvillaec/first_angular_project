d47fa737907cac9f1800addda9f09f75fc81e060
Andres Villavicencio <villadosa@hotmail.com>
2024-01-29 19:27:14 -0500

    feat: Set router-outlet  in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,18 +5,20 @@ import { RouterModule } from '@angular/router';
 @Component({
   standalone: true,
   selector: 'app-root',
+  imports: [HomeComponent, RouterModule],
   template: `
     <main>
+      <a [routerLink]="['/']">
       <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
       </header>
-      <section>
-        <app-home></app-home>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
       </section>
     </main> 
     `,
   styleUrls: ['./app.component.css'],
-  imports : [HomeComponent],
 })
 export class AppComponent {
   title = 'homes';
